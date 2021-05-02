7e2d816146054085eb99692fc946992a56485e42
Brian W Bush <code@functionally.io>
2021-05-02 10:52:13 -0600

    reorganized secrets

--- src/controller.js
+++ src/controller.js
@@ -1,13 +1,32 @@
 
+const Address    = require("./address.js"   )
+const Blockfrost = require("./blockfrost.js")
+const Secrets    = require("./secrets.js"   )
+
 require("buffer")
 
 
+Blockfrost.setKey(Secrets.blockfrostKey)
+
+
+const theVerificationKey = Address.makeVerificationKey(Secrets.theSigningKey)
+
+const theAddress = Address.makeAddress(theVerificationKey)
+
+
 module.exports = {
+
   Address     : require("./address.js"    )
 , Blockfrost  : require("./blockfrost.js" )
 , Encryption  : require("./encryption.js" )
-, Secrets     : require("./secrets.js"    )
 , Transaction : require("./transaction.js")
-, Buffer      : Buffer
-, Cardano     : require("@emurgo/cardano-serialization-lib-asmjs/cardano_serialization_lib.js")
+
+, Buffer  : Buffer
+, Cardano : require("@emurgo/cardano-serialization-lib-asmjs/cardano_serialization_lib.js")
+
+, thePassword        : Secrets.thePassword
+, outputAddress      : Secrets.outputAddress
+, theSigningKey      : Secrets.theSigningKey
+, theVerificationKey : theVerificationKey
+, theAddress         : theAddress
 }
